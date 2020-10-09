2e064654aaa755e5430a2581098d0d3751cd8b3e
JeffreyAJBailey <68293235+JeffreyAJBailey@users.noreply.github.com>
2020-10-09 13:22:11 -0400

    Add files via upload

--- PYTHON/TX/interleave_symbs.py
+++ PYTHON/TX/interleave_symbs.py
@@ -7,21 +7,21 @@ w96 = np.array(w96).reshape(96)
 w192 = np.array(w192).reshape(192)
 w288 = np.array(w288).reshape(288)
 
-a48 = [(w48[j]-1) for j in range(0,48)]
-a96 = [(w96[j]-1) for j in range(0,96)]
-a192 = [(w192[j]-1) for j in range(0,192)]
-a288 = [(w288[j]-1) for j in range(0,288)]
+a48 = [(w48[j]-1) for j in range(0, 48)]
+a96 = [(w96[j]-1) for j in range(0, 96)]
+a192 = [(w192[j]-1) for j in range(0, 192)]
+a288 = [(w288[j]-1) for j in range(0, 288)]
 
 
 def interleave_symbs(bits, NCBPS):
     bits_inter = np.zeros(NCBPS)    
     if NCBPS == 48:
-        bits_inter = [bits[j] for j in a48]
+        bits_inter = [bits[i] for i in a48]
     elif NCBPS == 96:
-        bits_inter = [bits[j] for j in a96]
+        bits_inter = [bits[i] for i in a96]
     elif NCBPS == 192:
-        bits_inter = [bits[j] for j in a192]
+        bits_inter = [bits[i] for i in a192]
     elif NCBPS == 288:
-        bits_inter = [bits[j] for j in a288]
+        bits_inter = [bits[i] for i in a288]
 
     return bits_inter
--- PYTHON/TX/top_tx.py
+++ PYTHON/TX/top_tx.py
@@ -1,5 +1,4 @@
 import numpy as np
-import math as m
 import constants as c
 from sig_field import sig_field
 from payload_bits import payload_bits
@@ -7,6 +6,7 @@ from scrambler import scrambler
 from conv_enc import conv_enc
 from pilot_generator import pilot_generator
 from interleave_symbs import interleave_symbs
+from modulate_symbs import modulate_symbs
 
 
 def top_tx(R, MSC, PPDU_length, FC, MAC1, MAC2, MAC3, TIME_STAMP):
@@ -27,9 +27,12 @@ def top_tx(R, MSC, PPDU_length, FC, MAC1, MAC2, MAC3, TIME_STAMP):
     lts_norm = lts / np.sqrt(np.dot(np.conjugate(np.transpose(lts)), lts))
     lts_norm = np.array(lts_norm)
     ltf = np.append(lts_norm[32:63+1], lts_norm)
-    ltf = np.append(ltf, sts_norm[32:63 + 1])
+    ltf = np.append(ltf, lts_norm)
 
     sig, NCBPS, NDBPS = sig_field(R, MSC, PPDU_length)
+    sig = sig/np.sqrt(np.dot(np.conjugate(np.transpose(sig)), sig))
+    preamble = np.append(stf, np.append(ltf, sig))
+
 
     ##################################
     # PAYLOAD
@@ -44,14 +47,23 @@ def top_tx(R, MSC, PPDU_length, FC, MAC1, MAC2, MAC3, TIME_STAMP):
 
     # samples for symbols
     ppdu_samples = np.zeros(int(80*Nsym))
+    ppdu_samples = ppdu_samples + 0j
 
     # pilot polarity for symbols:
     pilot_polarity = pilot_generator(int(Nsym + 1))
 
-    for n in range(0, int(Nsym)):
-        bits_inter = interleave_symbs(bits_encoded[(n * NCBPS) + 0: (n * NCBPS) + NCBPS], NCBPS)
+    for n in range(1, int(Nsym)+1):
+        bits_inter = interleave_symbs(bits_encoded[((n-1) * NCBPS) + 0: ((n-1) * NCBPS) + NCBPS], NCBPS)
+        iq_symb_fft = modulate_symbs(bits_inter, pilot_polarity[n], MSC)
+        iq_symb = np.fft.ifft(np.fft.fftshift(iq_symb_fft))
+        iq_symb_norm = iq_symb/np.sqrt(np.dot(np.conjugate(np.transpose(iq_symb)), iq_symb))
+        cyclic_prefix = iq_symb_norm[48:64]
+        iq_symb_80 = np.append(cyclic_prefix, iq_symb_norm)
+        ppdu_samples[((n-1)*(len(cyclic_prefix)+symb_len)+0):(n-1)*(len(cyclic_prefix)+symb_len)+(len(cyclic_prefix)+symb_len)] = iq_symb_80
         temp = 0
-    return
 
-top_tx('1/2', '16-QAM', 2000, '8000', 'BEAC0907', 'BEAC0907', 'BEAC0907', 1000)
+    s = np.append(preamble, ppdu_samples)
+    return s
+
+signal = top_tx('1/2', '16-QAM', 2000, '0800', 'BEAC0907', 'BEAC0907', 'BEAC0907', 1000)
 
